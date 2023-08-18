6d8c930ebec24583caf3eb6d0318981c6a7e9e1a
neharaut00 <neharaut372@gmail.com>
2023-08-19 04:53:48 +0530

    profies added to external files

---
+++ src/app/Profiles.ts
@@ -0,0 +1,7 @@
+export const profiles = [
+    { name: 'Mumbai', latitude: 19.0760, longitude: 72.8777 },
+    { name: 'Bangalore', latitude: 12.9716, longitude: 77.5946 },
+    { name: 'Hyderabad', latitude: 17.3850, longitude: 78.4867 },
+    { name: 'Kolkata', latitude: 22.5726, longitude: 88.3639 },
+    // Add more profiles as needed
+  ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { profiles } from './Profiles';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,15 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LocationMapping';
-  profiles = [
-    { name: 'Mumbai', latitude: 19.0760, longitude: 72.8777 },
-    { name: 'Bangalore', latitude: 12.9716, longitude: 77.5946 },
-    { name: 'New York', latitude: 40.7128, longitude: -74.0060 },
-    { name: 'Mumbai', latitude: 19.0760, longitude: 72.8777 },
-    { name: 'Bangalore', latitude: 12.9716, longitude: 77.5946 },
-    // Add more profiles with lat/long values
-    
-  ];
+  profiles = profiles;
   selectedProfile: any; // Store the selected profile
 
   handleProfileSelection(profile: any) {
